e2d2120375fd21164a9f39294fef73a664bf6992
Vincent Wochnik <v.wochnik@gmail.com>
2015-11-22 12:38:21 +0100

    update algorithm to match only alphanumeric characters and support non-ASCII characters

--- lib/jekyll-email-protect.rb
+++ lib/jekyll-email-protect.rb
@@ -1,9 +1,17 @@
 module JekyllEmailProtect
   module EmailProtectionFilter
-    def protect_email(str)
-      return str if !str.ascii_only?
 
-      str.chars.map{ |c| c =~ /[\.\-+@]/ ? c : c.ord.to_s(16).upcase.rjust(2, '0').prepend('%') }.to_a.join
+    # Percent-encode alphanumeric characters of an email address
+    def protect_email(input)
+      input.to_s.chars.inject("") do |result, char|
+        if char =~ /\p{Alnum}/
+          char.bytes.inject(result) do |result, byte|
+            result << byte.to_s(16).upcase.rjust(2, '0').prepend('%')
+          end
+        else
+          result << char
+        end
+      end
     end
   end
 end
